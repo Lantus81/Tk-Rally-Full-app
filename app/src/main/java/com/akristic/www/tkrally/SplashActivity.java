db0bc23997c25a868e9f80501a0bc8444a04d444
Lantus81 <ante.kristic81@gmail.com>
2017-03-19 20:15:58 +0100

    Fix Crash of app

--- app/src/main/java/com/akristic/www/tkrally/SplashActivity.java
+++ app/src/main/java/com/akristic/www/tkrally/SplashActivity.java
@@ -2,27 +2,25 @@ package com.akristic.www.tkrally;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
-import android.os.Build;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
+
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import static android.R.attr.duration;
 
 public class SplashActivity extends AppCompatActivity {
     AnimationDrawable logoAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//Remove title bar
+        //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-//Remove notification bar
+        //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
@@ -43,7 +41,7 @@ public class SplashActivity extends AppCompatActivity {
 
                 } finally {
 
-                    Intent i = new Intent(SplashActivity.this,NavigationScoreKeeperActivity.class);
+                    Intent i = new Intent(SplashActivity.this, NavigationScoreKeeperActivity.class);
                     startActivity(i);
                     finish();
                 }
@@ -55,7 +53,7 @@ public class SplashActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
 
-        if(hasFocus) {
+        if (hasFocus) {
             logoAnimation.start();
 
         }
